bacfa35f80067b27b77db778c06dd145b879c8c3
Adila Nur Lathifah <strawberrytata3031@gmail.com>
2023-09-25 18:36:16 +0700

    push challenge 03

--- app/src/main/java/org/adilanur/challenge02/MainActivity.kt
+++ app/src/main/java/org/adilanur/challenge02/MainActivity.kt
@@ -2,10 +2,20 @@ package org.adilanur.challenge02
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("MainActivity", "Jumlah data: " + getData().size)
+    }
+
+    private fun getData(): List<Menu> {
+        return listOf(
+            Menu("Ayam Goreng", 12000, R.drawable.fried_chicken)
+        )
+
     }
 }
