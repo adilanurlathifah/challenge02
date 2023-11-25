656a3ec6b4aaa0b8dc166d9da217783fedf768a4
Adila Nur Lathifah <strawberrytata3031@gmail.com>
2023-11-25 15:28:59 +0700

    fixing ch 5

--- app/src/main/java/org/adilanur/challenge02/MainActivity.kt
+++ app/src/main/java/org/adilanur/challenge02/MainActivity.kt
@@ -21,17 +21,6 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val crashButton = Button(this)
-        crashButton.text = "Test Crash"
-        crashButton.setOnClickListener {
-            throw RuntimeException("Test Crash") // Force a crash
-        }
-
-        addContentView(crashButton, ViewGroup.LayoutParams(
-            ViewGroup.LayoutParams.MATCH_PARENT,
-            ViewGroup.LayoutParams.WRAP_CONTENT))
-
-
         navController = findNavController(R.id.nav_host)
 
         navController.addOnDestinationChangedListener { _, destination, _ ->
